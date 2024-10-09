6852f001fd35ac1a12fe7f264b8020beea5c5798
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-10-09 11:54:28 +0000

    Backend API Test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import { client } from "#/lib/client";
 
 const Page: NextPage = async () => {
+  const api = await client.api.v1.$get();
+  const message = api.text()
 
   return (
     <div className="flex flex-col gap-4">
       マンホールは大体円形だっ!
+
+      API通信テスト{message}
     </div>
   );
 };
