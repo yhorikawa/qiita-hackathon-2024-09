3485682bcb4e58b20d517511c4f9290d18b638f3
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-10-16 17:40:02 +0000

    ✨ setup backend

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -1,7 +1,5 @@
 import type { D1Database } from "@cloudflare/workers-types/experimental";
 import { Hono } from "hono";
-import conversationsApi from "./conversations";
-import documentsApi from "./documents";
 
 export type Bindings = {
   DB: D1Database;
@@ -12,7 +10,5 @@ const api = new Hono<{ Bindings: Bindings }>()
   .get("/", (c) => {
     return c.text("Hello Hono!");
   })
-  .route("/conversations", conversationsApi)
-  .route("/documents", documentsApi);
 
 export default api;
