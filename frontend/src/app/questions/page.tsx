11fe4e00b60b2faed76eec73653342fb7834f40a
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-10-19 08:37:18 +0000

    ✨ use react hook form

--- frontend/src/app/questions/page.tsx
+++ frontend/src/app/questions/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type { NextPage } from "next";
+import { Fragment } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { QUESTIONS } from "#/constants/questions";
 
 const schema = z.object({
-  answer1: z.string(),
-  answer2: z.string(),
-  answer3: z.string(),
-  answer4: z.string(),
-  answer5: z.string(),
+  extraversion: z.string().min(1),
+  agreeableness: z.string().min(1),
+  conscientiousness: z.string().min(1),
+  neuroticism: z.string().min(1),
+  openness: z.string().min(1),
 });
-
-const QUESTIONS = [
-  "Q1. シチューとカレーは飲み物ですか",
-  "Q2. 味噌汁は飲み物ですか",
-  "Q3. ハヤシライスは飲み物ですか",
-  "Q4. ぜんざいは飲み物ですか",
-  "Q5. コーンスープは飲み物ですか",
-];
 
 type FormValues = z.infer<typeof schema>;
 
@@ -26,9 +20,10 @@ const Page: NextPage = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormValues>({
     resolver: zodResolver(schema),
+    mode: "onChange",
   });
 
   const onSubmit = (data: FormValues) => {
@@ -38,20 +33,26 @@ const Page: NextPage = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <h2 className="flex items-center justify-center mb-48">
-          <span className="text-xl text-black font-bold">{QUESTIONS[0]}</span>
-        </h2>
-        <div>
-          <p className="text-sm font-medium">回答内容</p>
-          <input type="textarea" {...register("answer1")} />
-        </div>
-      </div>
-      <input type="textarea" {...register("answer2")} />
-      <input type="textarea" {...register("answer3")} />
-      <input type="textarea" {...register("answer4")} />
-      <input type="textarea" {...register("answer5")} />
-      <button type="submit">Submit</button>
+      {QUESTIONS.map(({ type, id, text }) => (
+        <Fragment key={id}>
+          <div>
+            <h2 className="flex items-center justify-center mb-48">
+              <span className="text-xl text-black font-bold">{text}</span>
+            </h2>
+            <div>
+              <p className="text-sm font-medium">回答内容</p>
+              <input type="textarea" {...register(type)} />
+            </div>
+          </div>
+        </Fragment>
+      ))}
+      <button
+        type="submit"
+        disabled={!isValid}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        {isValid ? "この回答で守護霊を作成する" : "未入力の項目があります"}
+      </button>
     </form>
   );
 };
