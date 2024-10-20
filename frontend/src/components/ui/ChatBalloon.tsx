264b64799874cf456a8ce486cfbd6fd317677536
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-10-20 04:26:18 +0000

    Update Link href in room page component and fix top style

--- frontend/src/components/ui/ChatBalloon.tsx
+++ frontend/src/components/ui/ChatBalloon.tsx
@@ -3,19 +3,26 @@ import { twMerge } from "tailwind-merge";
 type Props = {
   message: string;
   position: "left" | "right";
+  isAi?: boolean;
 };
 
-export const ChatBallon = ({ message, position }: Props) => {
+export const ChatBallon = ({ message, position, isAi }: Props) => {
   return (
     <div
       className={twMerge([
-        "flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-white border border-solid",
+        "flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 border border-solid",
         position === "left"
           ? "rounded-e-xl rounded-es-xl"
           : "rounded-ee-xl rounded-s-xl",
+        isAi ? "bg-white" : "bg-indigo-600",
       ])}
     >
-      <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
+      <p
+        className={twMerge([
+          "text-sm font-normal py-2.5",
+          isAi ? "text-gray-900" : "text-white",
+        ])}
+      >
         {message}
       </p>
     </div>
